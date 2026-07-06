8530ac231c53e837588f40aaaf755532c667b327
Banita Mohanta <binibani14@gmail.com>
2026-07-06 21:14:11 +0530

    Time: 95 ms (7.58%), Space: 12.7 MB (40.91%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeCoveredIntervals(self, intervals):
+        n = len(intervals)
+        covered = [False] * n
+
+        for i in range(n):
+            for j in range(n):
+                if i != j:
+                    if (intervals[j][0] <= intervals[i][0] and
+                        intervals[i][1] <= intervals[j][1]):
+                        covered[i] = True
+                        break
+
+        return covered.count(False)
+        
