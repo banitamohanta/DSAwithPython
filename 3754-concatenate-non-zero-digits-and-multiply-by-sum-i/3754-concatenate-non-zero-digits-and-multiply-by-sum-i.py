d1909d92466e73c148ecf8c4c0e50f45ef7e15e5
Banita Mohanta <binibani14@gmail.com>
2026-07-07 22:18:24 +0530

    Time: 9 ms (8.99%), Space: 12.4 MB (44.94%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sumAndMultiply(self, n):
+        x = 0
+        s = 0
+        for ch in str(n):
+            if ch != '0':
+                d = int(ch)
+                x = x * 10 + d
+                s += d
+        return x * s
+        
